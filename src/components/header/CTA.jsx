25ae4b452705d796abff9c46b6c72397877412f5
berkeperim <berke.perim@gmail.com>
2023-04-08 21:49:23 +0300

    Updates

--- src/components/header/CTA.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CV from '../../assets/Berke_Perim_Resume.pdf'
-const CTA = () => {
-  return (
-      <a href={CV} className="btn cta-btn" download>Download CV</a>
-  )
-}
-
-export default CTA
