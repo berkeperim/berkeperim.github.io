0f6f76c0e3813b3b61dca443fa73ccf12aa537fc
berkeperim <berke.perim@gmail.com>
2023-04-13 17:09:32 +0300

    initial commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
           <div className="about__content">
             <p>My name is Berke and I enjoy creating things that live on the internet. I'm a Turkey based Frontend Developer and a bit of Backend Developer :)</p>
             <p>I have more than 5 years of experience as a Frontend Developer. I have had the privilage of working at <b>many advertising agencies</b>, <b>a company that develops a CRM project - smart call system - with small ERP structure</b>, <b>a company that develops booking applications</b> and currently <b>a huge startup that produces personalization, recommendation and predictive technology for e-commerce brands.</b></p>
-            <p>I have hands on experience using front-end technologies and frameworks like <b>VanillaJS</b>, <b>ReactJS</b>, <b>jQuery</b> and back-end technologies and frameworks like <b>PHP</b>, <b>NodeJS</b>, <b>Laravel</b>. I am currently working on <b>ReactJS</b> and <b>NodeJS</b> to further my proficiency in these languages.</p> 
+            <p>I have hands on experience using front-end technologies and frameworks like <b>Javascript</b>, <b>ReactJS</b>, <b>jQuery</b> and back-end technologies and frameworks like <b>PHP</b>, <b>NodeJS</b>, <b>Laravel</b>. I am currently working on <b>ReactJS</b> and <b>NodeJS</b> to further my proficiency in these languages.</p> 
             <a href="#contact" className="btn btn-primary">Let's Talk</a>
           </div>
         </div>
